d20ca456bd3f266582bdca79ecdaa2de5612c2a8
Markus Pikkanen <markus.pikkanen@gmail.com>
2018-06-05 11:52:28 +0300

    added babel to server development

--- server/server.js
+++ server/server.js
@@ -1,12 +1,9 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
 const server = express();
-const app = require('./app');
-const posts = require('.');
 
 const port = process.env.PORT || 8080;
 
 server.use(express.static(path.resolve(__dirname, '../client/')));
-server.use('/api', posts);
 
 server.listen(port, () => console.log('Listening on port 8080'));
