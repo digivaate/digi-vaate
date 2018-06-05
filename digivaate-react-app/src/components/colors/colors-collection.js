c42b48decd7e982984faeb2a745ca8ee18c220f4
Khang Nguyen <ken.nguyen.bk62@gmail.com>
2018-06-05 15:58:20 +0700

    Colors component: create and view

---
+++ digivaate-react-app/src/components/colors/colors-collection.js
@@ -0,0 +1,70 @@
+import React,{Component} from 'react';
+import ColorPage from './create-color';
+import { Card, Icon, Avatar } from 'antd';
+const { Meta } = Card;
+
+class ColorCollection extends Component{
+    constructor(props){
+        super(props);
+        this.state ={};
+    }
+
+    colorCard = [];
+
+    getColorCard(values){
+        this.colorCard = values;
+        this.setState({});
+    }
+
+
+    render() {
+        if(this.colorCard.length === 0){
+            return (
+            <div>
+                <ColorPage colorCard={values => this.getColorCard(values) }/>
+            </div>
+            )
+        }
+        else {
+            const colorCard = this.colorCard.map(element => {
+                return(
+                <Card.Grid
+                    style={{
+                        width: '150px',
+                        height: '150px',
+                        textAlign: 'center',
+                        backgroundColor: element.hexCode,
+                        borderRadius: '4px'
+                    }}
+                    actions={[<Icon type="setting"/>, <Icon type="edit"/>, <Icon type="ellipsis"/>]}
+                    key={element.name}
+                >
+                    <Meta
+                        title={element.name}
+                        description={element.colorCode}
+                        style={{
+                            width: '100%',
+                            height: '60%',
+                            textAlign: 'center',
+                            backgroundColor: 'white',
+                            marginTop: '60px',
+                            borderRadius: '4px'
+                        }}
+                    />
+                </Card.Grid>
+                )
+            });
+            return (
+                <div>
+                    <ColorPage colorCard={values => this.getColorCard(values) }/>
+                    <br/>
+                    <Card title="Color Collection">
+                        {colorCard}
+                    </Card>
+                </div>
+            )
+        }
+    }
+}
+
+export default ColorCollection;
---
+++ digivaate-react-app/src/components/colors/index.js
@@ -0,0 +1,19 @@
+import React,{Component} from 'react';
+import ColorPage from './create-color'
+import ColorCollection from './colors-collection'
+
+
+class ColorIndexPage extends Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <div>
+            <ColorCollection />
+            </div>
+        )
+    }
+}
+
+export default ColorIndexPage;
