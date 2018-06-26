1e2f2eb7f5ca5c73463aa45bf00792755420ebc6
Markus Pikkanen <markus.pikkanen@gmail.com>
2018-06-26 11:38:05 +0300

    season, query route

--- server/controllers/seasonController.js
+++ server/controllers/seasonController.js
@@ -22,6 +22,30 @@ exports.find_by_id = (req, res) => {
         });
 };
 
+exports.find_by_attribute = (req, res) => {
+    const params = {};
+    for(let attr in req.query) {
+        if (attr in models.Season.rawAttributes) {
+            params[attr] = req.query[attr];
+        } else {
+            res.status(500).json({
+                error: 'No attribute ' + attr + ' found'
+            });
+            return;
+        }
+    }
+    models.Season.findAll({
+        where: params
+    })
+        .then(ent => {
+            res.send(ent);
+        })
+        .catch(err => {
+            console.error(err);
+            res.stat(500).json(err);
+        });
+};
+
 exports.create = (req, res) => {
     models.Season.create(req.body, {
         include: [{
--- server/routes/seasonRoute.js
+++ server/routes/seasonRoute.js
@@ -3,6 +3,7 @@ const SeasonController = require('../controllers/seasonController');
 const router = express.Router();
 
 router.get('/', SeasonController.find_all);
+router.get('/search', SeasonController.find_by_attribute);
 router.get('/:id', SeasonController.find_by_id);
 router.post('/', SeasonController.create);
 router.patch('/:id', SeasonController.update);
