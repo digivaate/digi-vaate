504d4d0d86ea7b7e5a82ffff5cd15646828a327b
Markus Pikkanen <markus.pikkanen@gmail.com>
2018-08-08 09:52:10 +0300

    right model for controller

--- server/controllers/sizeController.js
+++ server/controllers/sizeController.js
@@ -2,7 +2,7 @@ const Controller = require("./Controller");
 const Models = require("../models/models");
 
 class SizeController extends Controller {
-    constructor() { super(Models.Product); }
+    constructor() { super(Models.Size); }
 
 }
 
